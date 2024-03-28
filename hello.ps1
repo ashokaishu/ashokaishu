ec02c27fa899385df5d84af195e8ac3d1dcf459b
ashokaishu <165010468+ashokaishu@users.noreply.github.com>
2024-03-28 14:59:01 +0530

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,1 +1,14 @@
-echo "Hello world"
+#!/usr/bin/pwsh -Command
+$countries = @(
+    'Argentina'
+    'USA'
+    'China'
+    'Francia'
+    'Costa Rica'
+    'Bolivia'
+    'Canada'
+)
+
+$random = Get-Random -Maximum 7
+
+Write-Output "Country: $($countries[$random])"
