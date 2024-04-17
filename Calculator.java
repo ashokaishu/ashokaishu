a983443ae61eb86ebbcc951c20c8d15848799673
ashokaishu <165010468+ashokaishu@users.noreply.github.com>
2024-04-17 12:51:28 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,31 +1,45 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+import java.io.PrintStream;
+
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
-    }
+   public Calculator() {
+   }
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
+   public int add(int var1, int var2) {
+      return var1 + var2;
+   }
 
-    public int multiply(int a, int b) {
-        return a * b;
-    }
+   public int subtract(int var1, int var2) {
+      return var1 - var2;
+   }
 
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        return a / b;
-    }
+   public int multiply(int var1, int var2) {
+      return var1 * var2;
+   }
 
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        int num1 = 10;
-        int num2 = 5;
+   public int divide(int var1, int var2) {
+      if (var2 == 0) {
+         throw new IllegalArgumentException("Cannot divide by zero");
+      } else {
+         return var1 / var2;
+      }
+   }
 
-        System.out.println("Addition: " + calculator.add(num1, num2));
-        System.out.println("Subtraction: " + calculator.subtract(num1, num2));
-        System.out.println("Multiplication: " + calculator.multiply(num1, num2));
-        System.out.println("Division: " + calculator.divide(num1, num2));
-    }
+   public static void main(String[] var0) {
+      Calculator var1 = new Calculator();
+      byte var2 = 10;
+      byte var3 = 5;
+      PrintStream var10000 = System.out;
+      int var10001 = var1.add(var2, var3);
+      var10000.println("Addition: " + var10001);
+      var10000 = System.out;
+      var10001 = var1.subtract(var2, var3);
+      var10000.println("Subtraction: " + var10001);
+      var10000 = System.out;
+      var10001 = var1.multiply(var2, var3);
+      var10000.println("Multiplication: " + var10001);
+      var10000 = System.out;
+      var10001 = var1.divide(var2, var3);
+      var10000.println("Division: " + var10001);
+   }
 }
